362056a7563193a0dcd2ba9f0187b5c5e5e1da48
ckurnias <caturksari@gmail.com>
2021-06-22 11:31:33 +0700

    a

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-08 08:34:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-08T08:34:28+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-15T07:31:48+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-15T07:31:56+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,22 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-02-24 06:37:12 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-02-24T06:37:12+00:00	INFO ::1	update	Update started by user Super User (223). Old version is 3.9.23.
+2021-02-24T06:37:16+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.24-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210224%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210224T063713Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=dc8459a9c662990e4c61566c30d91233f371fe4f51b4c08015d06a79b22c398e.
+2021-02-24T06:37:21+00:00	INFO ::1	update	File Joomla_3.9.24-Stable-Update_Package.zip downloaded.
+2021-02-24T06:37:21+00:00	INFO ::1	update	Starting installation of new version.
+2021-02-24T06:38:00+00:00	INFO ::1	update	Finalising installation.
+2021-02-24T06:38:01+00:00	INFO ::1	update	Deleting removed files and folders.
+2021-02-24T06:38:06+00:00	INFO ::1	update	Cleaning up after installation.
+2021-02-24T06:38:06+00:00	INFO ::1	update	Update to version 3.9.24 is complete.
+2021-03-08T08:34:54+00:00	INFO ::1	update	Update started by user Super User (223). Old version is 3.9.24.
+2021-03-08T08:34:57+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.25-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210308%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210308T083456Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=9b2960fec8873601f4b81231557ae2332fa695de3d35c47d86e251e7ed6f2176.
+2021-03-08T08:35:03+00:00	INFO ::1	update	File Joomla_3.9.25-Stable-Update_Package.zip downloaded.
+2021-03-08T08:35:03+00:00	INFO ::1	update	Starting installation of new version.
+2021-03-08T08:35:29+00:00	INFO ::1	update	Finalising installation.
+2021-03-08T08:35:30+00:00	INFO ::1	update	Deleting removed files and folders.
+2021-03-08T08:35:34+00:00	INFO ::1	update	Cleaning up after installation.
+2021-03-08T08:35:34+00:00	INFO ::1	update	Update to version 3.9.25 is complete.
